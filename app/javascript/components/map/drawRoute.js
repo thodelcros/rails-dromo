b07758dab91278d611f523ad10bcedd2130ac55d
Thomas Delcros <delcrosthomas@gmail.com>
2018-08-29 17:49:11 +0200

    Do not draw routes if there is no marker

--- app/javascript/components/map/drawRoute.js
+++ app/javascript/components/map/drawRoute.js
@@ -1,16 +1,20 @@
 function drawRoute(map, markers, styles) {
   let latlngPoint;
 
+  // Do not draw routes if there's no marker
+  if (markers.length == 0) { return; }
+
   const waypoints = markers.slice(1, -1).map (function(marker) {
     latlngPoint = new google.maps.LatLng(marker);
 
-    return google.maps.DirectionsWaypoint = {location: latlngPoint};
+    return google.maps.DirectionsWaypoint = { location: latlngPoint };
   });
 
   const origin = [markers[0].lat, markers[0].lng];
 
   const lastIndex   = markers.length - 1
   const destination = [markers[lastIndex].lat, markers[lastIndex].lng];
+
   map.drawRoute({
     origin:      origin,
     destination: destination,
